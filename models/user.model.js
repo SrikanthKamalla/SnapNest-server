e7cb0115adb93f29883dd2afbb9dc1b955e4a2d1
Srikanth Kamalla <srikanthkamalla2919@gmail.com>
2025-09-06 23:51:37 +0530

    decreased salting rounds to fast fetch

--- models/user.model.js
+++ models/user.model.js
@@ -54,7 +54,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   try {
-    const salt = await bcrypt.genSalt(15);
+    const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
   } catch (error) {
